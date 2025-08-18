a30cffd80684c7ab5f9dcfcca87d522cce808a99
Alexandrade01 <solidwinter96@outlook.com>
2025-08-17 22:31:30 -0300

    criacao entidade cliente

---
+++ src/main/java/curso/api/rest/model/Cliente.java
@@ -0,0 +1,97 @@
+package curso.api.rest.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	
+	@Column(name = "conta")
+	private String conta;
+	
+	//padrão será yyyy-MM-dd
+	@Column(name = "data_nascimento")
+	private LocalDate dataNascimento;
+	
+	@Column(name = "login")
+	private String login;
+	
+	@Column(name = "senha")
+	private String senha;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getConta() {
+		return conta;
+	}
+
+	public void setConta(String conta) {
+		this.conta = conta;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(Id, other.Id);
+	}
+
+	
+
+}
