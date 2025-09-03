241d1bde2f97d7290b613ad44b0d1ffc58a874a1
Alexandrade01 <solidwinter96@outlook.com>
2025-09-02 23:36:23 -0300

    adição de inicilizador tomcat na classe  principal

--- src/main/java/curso/api/rest/CursospringrestApiApplication.java
+++ src/main/java/curso/api/rest/CursospringrestApiApplication.java
@@ -4,25 +4,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
-@EntityScan(basePackages = {"curso.api.rest.model"})
+//@SpringBootApplication
+//@EntityScan(basePackages = {"curso.api.rest.model"})
+
+//public class CursospringrestApiApplication {
+//
+//	public static void main(String[] args) {
+//
+//		SpringApplication.run(CursospringrestApiApplication.class, args);
+//	}
+//
+//}
+
 //@ComponentScan(basePackages = {"curso.*"})
 //@EnableJpaRepositories(basePackages = {"curso.api.rest.repository"})
 //@EnableTransactionManagement
 //@EnableWebMvc
 //@RestController
 //@EnableAutoConfiguration
-public class CursospringrestApiApplication {
+@SpringBootApplication
+@EntityScan(basePackages = {"curso.api.rest.model"})
+public class CursospringrestApiApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CursospringrestApiApplication.class);
+    }
 
-		SpringApplication.run(CursospringrestApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CursospringrestApiApplication.class, args);
+    }
 }
+
